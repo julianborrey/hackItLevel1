1ffdb2d6a825091862fcc1a6e652aa06ceab158d
julianborrey <julian.borrey@duke.edu>
2014-06-27 20:28:31 -0700

    updated .c

--- strToInts.c
+++ strToInts.c
@@ -1,14 +1,13 @@
-//charToInt.c
+//strToInts.c
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
 int main(int argc, char* argv[]){
    
    int i = 0;
-   while(argv[1][i] != '\0'){
-      printf("%d", (int) argv[1][i]);
+   while(argv[1][i] != '\0'){          //until end of string
+      printf("%d", (int) argv[1][i]);  //print out int of ith char
       i++;
    }
    printf("\n");
